e410e687675f5149033bdcd5ca1677df803e5b9c
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-10-02 08:29:16 +0200

    changing scripts

--- meta/io.murano/Resources/murano-init.sh
+++ meta/io.murano/Resources/murano-init.sh
@@ -2,18 +2,70 @@
 
 set -eu
 
-if [ -f "/etc/redhat-release" ]; then
-  wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-  rpm -ivh epel-release-6-8.noarch.rpm
-  yum install subversion git-core wget make gcc python-pip python-dev python-setuptools python-virtualenv -y
-# Check for debian_version
-elif [ -f "/etc/debian_version" ]; then
-  apt-get update 
-  apt-get install subversion  git-core wget make gcc python-pip python-dev python-setuptools python-virtualenv -y
 
-else
-  install-packages git-core wget make gcc python-pip python-dev python-setuptools python-virtualenv
+# We obtain the Operative system distribution
+if test -f "/etc/lsb-release" && grep -q DISTRIB_ID /etc/lsb-release; then
+  platform=`grep DISTRIB_ID /etc/lsb-release | cut -d "=" -f 2 | tr '[A-Z]' '[a-z]'`
+  platform_version=`grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f 2`
+elif test -f "/etc/debian_version"; then
+  platform="debian"
+  platform_version=`cat /etc/debian_version`
+elif test -f "/etc/redhat-release"; then
+  platform=`sed 's/^\(.\+\) release.*/\1/' /etc/redhat-release | tr '[A-Z]' '[a-z]'`
+  echo $platform
+  platform_version=`sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release`
+  echo $platform_version
 fi
+
+# We obtain the Operative system version
+
+major_version=`echo $platform_version | cut -d. -f1`
+case $platform in
+  # FIXME: should remove this case statement completely
+  "el")
+    # FIXME:  "el" is deprecated, should use "redhat"
+    platform_version=$major_version
+    ;;
+  "debian")
+    # FIXME: remove client-side yolo here
+    case $major_version in
+      "5") platform_version="6";;  # FIXME: need to port this "reverse-yolo" into platform.rb
+      "6") platform_version="6";;
+      "7") platform_version="6";;
+    esac
+    ;;
+  "freebsd")
+    platform_version=$major_version
+    ;;
+  "sles")
+    platform_version=$major_version
+    ;;
+  "suse")
+    platform_version=$major_version
+    ;;
+esac
+
+
+if [ ${platform:0:6} == 'centos' ]; then
+    echo $platform_version
+    if [[ ${platform_version:0:1} = "6" ]]; then
+
+        rpm -ivh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm 
+        rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+    fi
+    if [[ ${platform_version:0:1} = "7" ]]; then
+        rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+        rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+    fi
+    if [[ ${platform_version:0:1} = "5" ]]; then
+       rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm
+    fi
+    yum -y install subversion git-core wget make gcc python-pip python-dev python-setuptools python-virtualenv  puppet
+else
+    apt-get update 
+    apt-get install subversion  git-core wget make gcc python-pip python-dev python-setuptools python-virtualenv puppet -y
+fi
+
 
 name=murano-agent
 svc_root=${DIB_MURANO_AGENT_SVC_ROOT:-/opt/stack/$name}
@@ -42,7 +94,7 @@ pushd ${svc_root}
 ${install_dir}/bin/oslo-config-generator --config-file ${svc_root}/etc/oslo-config-generator/muranoagent.conf
 popd
 
-#cp ${svc_root}/etc/muranoagent/muranoagent.conf.sample /etc/murano/agent.conf.sample
+cp ${svc_root}/etc/muranoagent/muranoagent.conf.sample /etc/murano/agent.conf.sample
 
 dir_folder=${svc_root}/contrib/elements/murano-agent/install.d/
 # install upstart script for murano-agent
@@ -52,6 +104,12 @@ fi
 if [ -d "/etc/systemd/system/" ]; then
     install -D -g root -o root -m 0755 ${dir_folder}/murano-agent.service /etc/systemd/system/
 fi
-cd /etc/init.d
+
+if [ ${platform:0:6} == 'centos' ]; then
+    if [[ ${platform_version:0:1} = "6" ]]; then
+     cd /etc/init.d
 wget http://repositories.testbed.fi-ware.org/webdav/murano-agent 
-chmod 777 murano-agent
+chmod 777 murano-agent 
+   fi
+fi
+service murano-agent start
